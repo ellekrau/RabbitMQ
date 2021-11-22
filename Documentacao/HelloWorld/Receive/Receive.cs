b0637c2aceaa653fb1ae04f209604f54d6862bbf
ellekrau <ellekrau91@gmail.com>
2021-11-21 23:00:45 -0300

    Initial commit

---
+++ Documentacao/HelloWorld/Receive/Receive.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Receive
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        
+        using (var channel = factory.CreateConnection().CreateModel())
+        {
+            var queueName = "hello";
+
+             channel.QueueDeclare
+             (
+                queue: queueName,
+                 durable: false,
+                 exclusive: false,
+                 autoDelete: true,
+                 arguments: null
+             );
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                Console.WriteLine($" Received: {message}");
+            };     
+
+            channel.BasicConsume(
+                queue: queueName,
+                autoAck: true,
+                consumer: consumer
+            );            
+        }
+    }
+
+}
---
+++ Documentacao/HelloWorld/Send/Send.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+class Send
+{
+    public static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost"}; 
+
+        using (var channel = factory.CreateConnection().CreateModel())
+        {
+            var queueName = "hello";
+
+            channel.QueueDeclare(
+                queue: queueName,
+                durable: false,
+                exclusive: false,
+                arguments: null
+            );
+
+            var message = "Jaine";
+            
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: queueName,
+                basicProperties: null,
+                body: Encoding.UTF8.GetBytes(message)
+            );
+
+            Console.WriteLine($" Sent message: {message}");
+        }
+    }
+    
+}
+
