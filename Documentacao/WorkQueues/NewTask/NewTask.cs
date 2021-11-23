ebf1720236cc9ef10eb544ed593be671061d9790
ellekrau <ellekrau91@gmail.com>
2021-11-22 21:59:22 -0300

    WorkQueues implementation

---
+++ Documentacao/WorkQueues/NewTask/NewTask.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+class NewTask
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        using (var channel = factory.CreateConnection().CreateModel())
+        {
+            var routingKey = "task_queue";
+
+            channel.QueueDeclare(
+                queue: routingKey,
+                durable: false,
+                exclusive: false,
+                autoDelete: true
+            );
+
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            while(true)
+            {
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: routingKey,
+                    basicProperties: properties,
+                    body: Encoding.UTF8.GetBytes(GetMessage())
+                );
+            }
+        }
+    }
+
+    static string GetMessage()
+    {
+        var message = string.Empty;
+
+        do
+        {
+            Console.Write(" Insert a message: ");
+            message = Console.ReadLine();
+        } while (string.IsNullOrWhiteSpace(message));
+        
+        return message;
+    }
+}
---
+++ Documentacao/WorkQueues/Worker/Worker.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+class Worker
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+
+        using (var channel = factory.CreateConnection().CreateModel())
+        {
+            var routingKey = "task_queue";
+
+            channel.QueueDeclare(
+                queue: routingKey,
+                durable: false,
+                exclusive: false,
+                autoDelete: true
+            );
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                Console.WriteLine($" Received: {message}");
+
+                var sleepTime = (message.Split(".").Length - 1) * 1000;
+                Console.WriteLine($" Sleep time: {sleepTime} milliseconds");
+                
+                Console.WriteLine(" Starting...");
+                Thread.Sleep(sleepTime);
+                Console.WriteLine(" Done!");
+
+                Console.WriteLine("======================================");
+            };
+
+            while (true)
+            {
+                channel.BasicConsume(queue: routingKey, autoAck: true, consumer: consumer);
+            }
+        }
+    }
+}
